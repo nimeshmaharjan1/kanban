8e485329331af9555c69e58a52c3652785eb6e62
nimeshmaharjan1 <maharjannimesh11@gmail.com>
2023-03-08 23:56:33 +0545

    ci: lint update :construction:

--- src/shared/layouts/main-layout/index.tsx
+++ src/shared/layouts/main-layout/index.tsx
@@ -2,8 +2,10 @@ import React, { PropsWithChildren } from 'react';
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
 import { RootState, useAppDispatch, useAppSelector } from '@/store';
-import { AiOutlineArrowLeft } from 'react-icons/ai';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 import { MdOutlineDashboard } from 'react-icons/md';
+import classNames from 'classnames';
+import { toggleSidebar } from '@/store/slices/shared';
 
 type P = PropsWithChildren;
 
@@ -15,6 +17,7 @@ const inter = Inter({
 });
 
 const MainSharedLayout = ({ children }: P) => {
+  const dispatch = useAppDispatch();
   const isSidebarCollapsed = useAppSelector((state: RootState) => state.sharedStore.isSidebarCollapsed);
   return (
     <>
@@ -22,8 +25,24 @@ const MainSharedLayout = ({ children }: P) => {
         <title>Tazk</title>
       </Head>
       <div className={`flex min-h-screen ${inter.className}`}>
-        <div className="bg-base-300 rounded-tr-box rounded-br-box absolute bottom-8 left-0 w-20 h-8"></div>
-        <div className="fixed h-full w-60 z-30 p-4 mt-[2.5px]">
+        <div
+          className={classNames(
+            'bg-base-300 cursor-pointer hover:text-white hover:shadow-lg rounded-tr-box rounded-br-box transition-all absolute bottom-8 left-0 w-16 h-8 items-center justify-center z-40 flex',
+            {
+              'opacity-100': isSidebarCollapsed,
+              'opacity-0': !isSidebarCollapsed,
+            }
+          )}
+          onClick={() => dispatch(toggleSidebar())}
+        >
+          <AiOutlineArrowRight className="text-sm"></AiOutlineArrowRight>
+        </div>
+        <div
+          className={classNames('fixed h-full w-60 z-30 p-4 mt-[2.5px] transition-all', {
+            'opacity-0 duration-75': isSidebarCollapsed,
+            'opacity-100 duration-1000': !isSidebarCollapsed,
+          })}
+        >
           <h1 className="font-[600] text-2xl text-primary p-1">Tazk</h1>
           <nav className="mt-12">
             <p className="uppercase tracking-wide opacity-50 text-sm font-[500]">All Boards (3)</p>
@@ -70,11 +89,15 @@ const MainSharedLayout = ({ children }: P) => {
               </button>
             </section>
           </nav>
-          <button className="btn btn-circle absolute bottom-8 -right-8 btn-sm">
+          <button className="btn btn-circle absolute bottom-8 -right-8 btn-sm" onClick={() => dispatch(toggleSidebar())}>
             <AiOutlineArrowLeft></AiOutlineArrowLeft>
           </button>
         </div>
-        <div className="flex flex-col ml-64 flex-1">
+        <div
+          className={classNames('flex flex-col ml-64 flex-1 transition-all duration-300', {
+            'ml-0': isSidebarCollapsed,
+          })}
+        >
           <header className="h-20 flex items-center px-6 sticky top-0 backdrop-blur !z-20">Header</header>
           <main className="flex-1 p-6 bg-base-200">{children}</main>
         </div>
